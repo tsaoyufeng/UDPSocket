87edba969e9097cb9f773bbb33a5cf2d4c9199f9
yvan <915794227@qq.com>
2016-11-09 21:38:46 +0800

    add comments
    add comments

--- UDPClient.py
+++ UDPClient.py
@@ -1,9 +1,13 @@
 from socket import *
+
+#hostname
 serverName = '192.168.182.128'
+#port number
 serverPort = 12000
+#create a udp socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 while True:
-	message = raw_input('>')
+	message = raw_input('-->')
 	clientSocket.sendto(message,(serverName,serverPort))
 	modifiedMessage,serverAddress = clientSocket.recvfrom(2048)
 	print modifiedMessage
--- UDPServer.py
+++ UDPServer.py
@@ -1,10 +1,12 @@
 from socket import *
+
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('',serverPort))
+print 'waiting for message...'
 while True:
-	print 'waiting for message...'
 	data, clientAddress = serverSocket.recvfrom(2048)
+	print 'I received',data
 	modifiedMessage = data.upper()
 	serverSocket.sendto(modifiedMessage, clientAddress)
 serverSocket.close()
